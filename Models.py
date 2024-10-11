515e4d511878de500daf7ec2e838c73f61c47a61
Pat-on-back <43055262+Pat-on-back@users.noreply.github.com>
2024-10-11 10:39:26 +0800

    Update Models.py

--- Models.py
+++ Models.py
@@ -367,24 +367,7 @@ input_vocab_size = tokenizer_pt.vocab_size + 2
 target_vocab_size = tokenizer_en.vocab_size + 2
 dropout_rate = 0.1
 
-class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
-    def __init__(self, d_model, warmup_steps=4000):
-        super(CustomSchedule, self).__init__()
-
-        self.d_model = d_model
-        self.d_model = tf.cast(self.d_model, tf.float32)
-
-        self.warmup_steps = warmup_steps
-
-    def __call__(self, step):
-        arg1 = tf.math.rsqrt(step)
-        arg2 = step * (self.warmup_steps**-1.5)
-
-        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
-
-learning_rate = CustomSchedule(d_model)
-
-optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.98,
                                      epsilon=1e-9)
 
 
@@ -437,3 +420,142 @@ if ckpt_manager.latest_checkpoint:
     ckpt.restore(ckpt_manager.latest_checkpoint)
     print('Latest checkpoint restored!!')
 
+# 该 @tf.function 将追踪-编译 train_step 到 TF 图中，以便更快地
+# 执行。该函数专用于参数张量的精确形状。为了避免由于可变序列长度或可变
+# 批次大小（最后一批次较小）导致的再追踪，使用 input_signature 指定
+# 更多的通用形状。
+
+train_step_signature = [
+    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+]
+
+@tf.function(input_signature=train_step_signature)
+def train_step(inp, tar):
+    tar_inp = tar[:, :-1]
+    tar_real = tar[:, 1:]
+
+    enc_padding_mask, combined_mask, dec_padding_mask = create_masks(
+        inp, tar_inp)
+
+    with tf.GradientTape() as tape:
+        predictions, _ = transformer(inp, tar_inp, True, enc_padding_mask,
+                                     combined_mask, dec_padding_mask)
+        loss = loss_function(tar_real, predictions)
+
+    gradients = tape.gradient(loss, transformer.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(tar_real, predictions)
+    
+for epoch in range(5):
+    start = time.time()
+
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+
+    # inp -> portuguese, tar -> english
+    for (batch, (inp, tar)) in enumerate(train_dataset):
+        train_step(inp, tar)
+
+        if batch % 50 == 0:
+            print('Epoch {} Batch {} Loss {:.4f} Accuracy {:.4f}'.format(
+                epoch + 1, batch, train_loss.result(),
+                train_accuracy.result()))
+
+    if (epoch + 1) % 5 == 0:
+        ckpt_save_path = ckpt_manager.save()
+        print('Saving checkpoint for epoch {} at {}'.format(
+            epoch + 1, ckpt_save_path))
+
+    print('Epoch {} Loss {:.4f} Accuracy {:.4f}'.format(
+        epoch + 1, train_loss.result(), train_accuracy.result()))
+
+    print('Time taken for 1 epoch: {} secs\n'.format(time.time() - start
+
+def evaluate(inp_sentence):
+    start_token = [tokenizer_pt.vocab_size]
+    end_token = [tokenizer_pt.vocab_size + 1]
+
+    # 输入语句是葡萄牙语，增加开始和结束标记
+    inp_sentence = start_token + tokenizer_pt.encode(inp_sentence) + end_token
+    encoder_input = tf.expand_dims(inp_sentence, 0)
+
+    # 因为目标是英语，输入 transformer 的第一个词应该是
+    # 英语的开始标记。
+    decoder_input = [tokenizer_en.vocab_size]
+    output = tf.expand_dims(decoder_input, 0)
+
+    for i in range(MAX_LENGTH):
+        enc_padding_mask, combined_mask, dec_padding_mask = create_masks(
+            encoder_input, output)
+
+        # predictions.shape == (batch_size, seq_len, vocab_size)
+        predictions, attention_weights = transformer(encoder_input, output,
+                                                     False, enc_padding_mask,
+                                                     combined_mask,
+                                                     dec_padding_mask)
+
+        # 从 seq_len 维度选择最后一个词
+        predictions = predictions[:, -1:, :]  # (batch_size, 1, vocab_size)
+
+        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+        # 如果 predicted_id 等于结束标记，就返回结果
+        if predicted_id == tokenizer_en.vocab_size + 1:
+            return tf.squeeze(output, axis=0), attention_weights
+
+        # 连接 predicted_id 与输出，作为解码器的输入传递到解码器。
+        output = tf.concat([output, predicted_id], axis=-1)
+
+    return tf.squeeze(output, axis=0), attention_weights
+
+def translate(sentence, plot=''):
+    result, attention_weights = evaluate(sentence)
+
+    predicted_sentence = tokenizer_en.decode(
+        [i for i in result if i < tokenizer_en.vocab_size])
+
+    print('Input: {}'.format(sentence))
+    print('Predicted translation: {}'.format(predicted_sentence))
+
+def plot_attention_weights(attention, sentence, result, layer):
+    fig = plt.figure(figsize=(16, 8))
+
+    sentence = tokenizer_pt.encode(sentence)
+
+    attention = tf.squeeze(attention[layer], axis=0)
+
+    for head in range(attention.shape[0]):
+        ax = fig.add_subplot(2, 4, head + 1)
+
+        # 画出注意力权重
+        ax.matshow(attention[head][:-1, :], cmap='viridis')
+
+        fontdict = {'fontsize': 10}
+
+        ax.set_xticks(range(len(sentence) + 2))
+        ax.set_yticks(range(len(result)))
+
+        ax.set_ylim(len(result) - 1.5, -0.5)
+
+        ax.set_xticklabels(['<start>'] +
+                           [tokenizer_pt.decode([i])
+                            for i in sentence] + ['<end>'],
+                           fontdict=fontdict,
+                           rotation=90)
+
+        ax.set_yticklabels([
+            tokenizer_en.decode([i])
+            for i in result if i < tokenizer_en.vocab_size
+        ],
+                           fontdict=fontdict)
+
+        ax.set_xlabel('Head {}'.format(head + 1))
+
+    plt.tight_layout()
+    plt.show()
+
+translate("este é o primeiro livro que eu fiz.", plot='decoder_layer4_block2')
+print ("Real translation: this is the first book i've ever done.")
